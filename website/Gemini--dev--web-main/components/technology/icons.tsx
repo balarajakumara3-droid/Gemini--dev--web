fdb11ca1271df28aa8d36757700205e783877bae
narayananitgithub <narayananitgithub@users.noreply.github.com>
2025-12-25 18:43:25 +0530

    fix: Update Flutter and Swift icons with official SVG paths

---
+++ website/Gemini--dev--web-main/components/technology/icons.tsx
@@ -0,0 +1,69 @@
+import React from 'react';
+
+export const ReactIcon = (props: React.SVGProps<SVGSVGElement>) => (
+    React.createElement("svg", { viewBox: "-11.5 -10.23174 23 20.46348", fill: "none", ...props },
+        React.createElement("circle", { cx: "0", cy: "0", r: "2.05", fill: "currentColor" }),
+        React.createElement("g", { stroke: "currentColor", strokeWidth: "1", fill: "none" },
+            React.createElement("ellipse", { rx: "11", ry: "4.2" }),
+            React.createElement("ellipse", { rx: "11", ry: "4.2", transform: "rotate(60)" }),
+            React.createElement("ellipse", { rx: "11", ry: "4.2", transform: "rotate(120)" })
+        )
+    )
+);
+
+export const NextJsIcon = (props: React.SVGProps<SVGSVGElement>) => (
+    React.createElement("svg", { viewBox: "0 0 180 180", fill: "none", ...props },
+        React.createElement("mask", { id: "mask0_next", maskUnits: "userSpaceOnUse", x: "0", y: "0", width: "180", height: "180", style: { maskType: "alpha" } },
+            React.createElement("circle", { cx: "90", cy: "90", r: "90", fill: "black" })
+        ),
+        React.createElement("g", { mask: "url(#mask0_next)" },
+            React.createElement("circle", { cx: "90", cy: "90", r: "90", fill: "black", stroke: "currentColor", strokeWidth: "6" }),
+            React.createElement("path", { d: "M149.508 157.52L69.142 54H54V125.97H66.1136V69.3836L139.999 164.845C143.333 162.614 146.509 160.165 149.508 157.52Z", fill: "currentColor" }),
+            React.createElement("rect", { x: "115", y: "54", width: "12", height: "72", fill: "currentColor" })
+        )
+    )
+);
+
+export const NodeJsIcon = (props: React.SVGProps<SVGSVGElement>) => (
+    React.createElement("svg", { viewBox: "0 0 32 32", fill: "none", ...props },
+        React.createElement("path", { d: "M16 2L3.5 9.5V22.5L16 30L28.5 22.5V9.5L16 2ZM14 24L7.5 20.5V11.5L14 15V24ZM16 13L9.5 9.5L16 5.5L22.5 9.5L16 13ZM18 24V15L24.5 11.5V20.5L18 24Z", fill: "currentColor" })
+    )
+);
+
+export const FlutterIcon = (props: React.SVGProps<SVGSVGElement>) => (
+    React.createElement("svg", { viewBox: "0 0 24 24", fill: "none", ...props },
+        React.createElement("path", { d: "M14.314 0L2.3 12 6 15.7 21.684.013h-7.357zm.014 11.072L7.857 17.53l6.47 6.47H21.7l-6.46-6.468 6.46-6.46h-7.37z", fill: "currentColor" })
+    )
+);
+
+export const SwiftIcon = (props: React.SVGProps<SVGSVGElement>) => (
+    React.createElement("svg", { viewBox: "0 0 24 24", fill: "none", ...props },
+        React.createElement("path", { d: "M7.508 0c-.287 0-.573 0-.86.002-.241.002-.483.003-.724.01-.132.003-.263.009-.395.015A9.154 9.154 0 0 0 4.348.15 5.492 5.492 0 0 0 2.85.645 5.04 5.04 0 0 0 .645 2.848c-.245.48-.4.972-.495 1.5-.093.52-.122 1.05-.136 1.576a35.2 35.2 0 0 0-.012.724C0 6.935 0 7.221 0 7.508v8.984c0 .287 0 .575.002.862.002.24.005.481.012.722.014.526.043 1.057.136 1.576.095.528.25 1.02.495 1.5a5.03 5.03 0 0 0 2.205 2.203c.48.244.97.4 1.498.495.52.093 1.05.124 1.576.138.241.007.483.009.724.01.287.002.573.002.86.002h8.984c.287 0 .573 0 .86-.002.241-.001.483-.003.724-.01a10.523 10.523 0 0 0 1.578-.138 5.322 5.322 0 0 0 1.498-.495 5.035 5.035 0 0 0 2.203-2.203c.245-.48.4-.972.495-1.5.093-.52.124-1.05.138-1.576.007-.241.009-.481.01-.722.002-.287.002-.575.002-.862V7.508c0-.287 0-.573-.002-.86a33.662 33.662 0 0 0-.01-.724 10.5 10.5 0 0 0-.138-1.576 5.328 5.328 0 0 0-.495-1.5A5.039 5.039 0 0 0 21.152.645 5.32 5.32 0 0 0 19.654.15a10.493 10.493 0 0 0-1.578-.138 34.98 34.98 0 0 0-.722-.01C17.067 0 16.779 0 16.492 0H7.508zm6.035 3.41c4.114 2.47 6.545 7.162 5.549 11.131-.024.093-.05.181-.076.272l.002.001c2.062 2.538 1.5 5.258 1.236 4.745-1.072-2.086-3.066-1.568-4.088-1.043a6.803 6.803 0 0 1-.281.158l-.02.012-.002.002c-2.115 1.123-4.957 1.205-7.812-.022a12.568 12.568 0 0 1-5.64-4.838c.649.48 1.35.902 2.097 1.252 3.019 1.414 6.051 1.311 8.197-.002C9.651 12.73 7.101 9.67 5.146 7.191a10.628 10.628 0 0 1-1.005-1.384c2.34 2.142 6.038 4.83 7.365 5.576C8.69 8.408 6.208 4.743 6.324 4.86c4.436 4.47 8.528 6.996 8.528 6.996.154.085.27.154.36.213.085-.215.16-.437.224-.668.708-2.588-.09-5.548-1.893-7.992z", fill: "currentColor" })
+    )
+);
+
+export const KotlinIcon = (props: React.SVGProps<SVGSVGElement>) => (
+    React.createElement("svg", { viewBox: "0 0 24 24", fill: "none", ...props },
+        React.createElement("path", { d: "M2 2H22L12 12L22 22H2V2Z", fill: "currentColor" })
+    )
+);
+
+export const FastApiIcon = (props: React.SVGProps<SVGSVGElement>) => (
+    React.createElement("svg", { viewBox: "0 0 24 24", fill: "none", ...props },
+        React.createElement("path", { d: "M12 2C6.48 2 2 6.48 2 12C2 17.52 6.48 22 12 22C17.52 22 22 17.52 22 12C22 6.48 17.52 2 12 2ZM12 20C7.58 20 4 16.42 4 12C4 7.58 7.58 4 12 4C16.42 4 20 7.58 20 12C20 16.42 16.42 20 12 20Z", fill: "currentColor", fillOpacity: "0.2" }),
+        React.createElement("path", { d: "M15 11H13V7L9 13H11V17L15 11Z", fill: "currentColor" })
+    )
+);
+
+export const SupabaseIcon = (props: React.SVGProps<SVGSVGElement>) => (
+    React.createElement("svg", { viewBox: "0 0 24 24", fill: "none", ...props },
+        React.createElement("path", { d: "M5.99988 12.0001L12.9999 2.00012L12.4999 10.5001H17.9999L10.9999 22.0001L11.4999 12.5001H5.99988Z", stroke: "currentColor", strokeWidth: "2", strokeLinejoin: "round" })
+    )
+);
+
+export const PythonIcon = (props: React.SVGProps<SVGSVGElement>) => (
+    React.createElement("svg", { viewBox: "0 0 128 128", fill: "none", ...props },
+        React.createElement("path", { d: "M63.63 9.05C33.53 9.05 31.75 22.37 31.75 22.37V34.57H59.95V38.9H22.7C22.7 38.9 9.17 37.83 9.17 62.48C9.17 84.85 22.1 84.45 22.1 84.45H32.65V71.22C32.65 71.22 32.22 55.48 47.93 55.48H73.35C73.35 55.48 84.62 54.38 84.62 43.12V23.77C84.62 23.77 86.6 9.05 63.63 9.05ZM45.65 19.33C48.05 19.33 50 21.28 50 23.68C50 26.08 48.05 28.03 45.65 28.03C43.25 28.03 41.3 26.08 41.3 23.68C41.3 21.28 43.25 19.33 45.65 19.33Z", fill: "currentColor" }),
+        React.createElement("path", { d: "M64.38 119.33C94.48 119.33 96.27 106.02 96.27 106.02V93.82H68.07V89.48H105.32C105.32 89.48 118.85 90.55 118.85 65.9C118.85 43.53 105.92 43.93 105.92 43.93H95.37V57.17C95.37 57.17 95.8 72.9 80.08 72.9H54.67C54.67 72.9 43.4 74 43.4 85.25V104.62C43.4 104.62 41.42 119.33 64.38 119.33ZM82.37 109.05C79.97 109.05 78.02 107.1 78.02 104.7C78.02 102.3 79.97 100.35 82.37 100.35C84.77 100.35 86.72 102.3 86.72 104.7C86.72 107.1 84.77 109.05 82.37 109.05Z", fill: "currentColor" })
+    )
+);
